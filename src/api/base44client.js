92fad34b04279fadb89138709c6f7d2c4af3cc07
vandehoekencitizens <elliotscottchan@gmail.com>
2026-02-13 11:33:29 +0800

    Update base44client.js

--- src/api/base44client.js
+++ src/api/base44client.js
@@ -1,9 +1,10 @@
 import { createClient } from '@base44/sdk';
-import { appParams } from '@/lib/app-params';
+// We changed the '@' to '..' so it can find the file in your folders
+import { appParams } from '../lib/app-params';
 
 const { appId, token, functionsVersion, appBaseUrl } = appParams;
 
-//Create a client with authentication required
+// Create a client with authentication required
 export const base44 = createClient({
   appId,
   token,
