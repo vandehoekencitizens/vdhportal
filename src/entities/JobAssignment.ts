1b87c0d6596f3149731c9aca6c0f27d45d0681d7
vandehoekencitizens <elliotscottchan@gmail.com>
2026-02-13 11:21:52 +0800

    Create JobAssignment.ts

---
+++ src/entities/JobAssignment.ts
@@ -0,0 +1,48 @@
+{
+  "name": "Job",
+  "type": "object",
+  "properties": {
+    "title": {
+      "type": "string",
+      "description": "Job title"
+    },
+    "department": {
+      "type": "string",
+      "description": "Government department"
+    },
+    "description": {
+      "type": "string",
+      "description": "Job description"
+    },
+    "salary": {
+      "type": "number",
+      "description": "Daily salary in VHS"
+    },
+    "location": {
+      "type": "string",
+      "description": "Job location"
+    },
+    "type": {
+      "type": "string",
+      "enum": [
+        "Full-time",
+        "Part-time",
+        "Contract"
+      ],
+      "default": "Full-time"
+    },
+    "status": {
+      "type": "string",
+      "enum": [
+        "open",
+        "closed"
+      ],
+      "default": "open"
+    }
+  },
+  "required": [
+    "title",
+    "department",
+    "description"
+  ]
+}
